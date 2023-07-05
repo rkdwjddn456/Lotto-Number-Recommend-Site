7eb83d071437985ab40520189fb9dfb7e7d9c8a3
rkdwjddn456 <dhrn456@gmail.com>
2023-07-05 13:47:31 +0900

    v1.0 폴더로 초기 코드 이동

--- v1.0/app.js
+++ v1.0/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const lottoRoutes = require('./routes/lotto');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/lotto', lottoRoutes);
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
